872ce5a08a749befc605621f1a6ca8183e803326
Veronka1212 <nikahendry@gmail.com>
2021-08-05 11:55:28 +0300

    tasks about composition

--- src/by/epam/prog_with_class/aggreg_compos/task2/Main.java
+++ src/by/epam/prog_with_class/aggreg_compos/task2/Main.java
@@ -1,5 +1,8 @@
 package by.epam.prog_with_class.aggreg_compos.task2;
 
+// Создать объект класса Автомобиль, используя классы Колесо, Двигатель. Методы: ехать, заправляться,
+//менять колесо, вывести на консоль марку автомобиля.
+
 public class Main {
     public static void main(String[] args) {
 
